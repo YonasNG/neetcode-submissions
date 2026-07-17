d33fb06074cb5e15b42f51bce8df2d2060b83ff8
Yonas Gebremariam <yonasgebremariam@college.harvard.edu>
2026-07-16 21:48:21 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 9
+print(type(variable))
+variable = 8.9
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hi"
+print(type(variable))
+variable = [1,3]
+print(type(variable))
